8a74d22d0ccd6c8a644c4d6e92d76bc072b0951f
Пэйнт <xcession@gmail.com>
2024-01-06 23:58:13 +0700

    refactor: Update project to Godot 4

--- scene1.gd
+++ scene1.gd
@@ -6,5 +6,5 @@ func _ready():
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://scene2.tscn")
+	get_tree().change_scene_to_file("res://scene2.tscn")
 	pass # Replace with function body.
--- scene2.gd
+++ scene2.gd
@@ -6,5 +6,5 @@ func _ready():
 
 
 func _on_Button_pressed():
-	get_tree().change_scene("res://scene1.tscn")
+	get_tree().change_scene_to_file("res://scene1.tscn")
 	pass # Replace with function body.
